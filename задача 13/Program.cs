89bdf18f1990d0cf8e65d19fa5866e56f93be0af
AlexandrBelyakov <belya0309@mail.ru>
2023-03-29 15:38:25 +0300

    Задача 13

--- задача 13/Program.cs
+++ задача 13/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+int num = new Random().Next(10, 2000);
+int result2 = (num / 100 ) ;
+int num2 = num;
+
+    if (result2 != 0)
+    {
+        while(num2 > 1000)
+        {
+            num2 = num2 /10;
+            
+        }
+        
+
+        Console.WriteLine($"{num} -> {(num2 % 100) %10}");
+
+    }
+    else 
+    {
+    Console.WriteLine($"{num} Третьего числа нет");
+    }
