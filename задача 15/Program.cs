f650709ae92e5d95e346fb3c6ebbbe78e2486c7f
AlexandrBelyakov <belya0309@mail.ru>
2023-03-29 21:58:29 +0300

    Задача 15

--- задача 15/Program.cs
+++ задача 15/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int ReadInt(string message) 
+{
+    Console.Write($"{message} >"); 
+    int value = Convert.ToInt32(Console.ReadLine()); 
+    return value;
+}
+
+int num = ReadInt("Сегодня рабочий день? Ввидите его цифру от 1 до 7: ");
+
+
+if(num <=5){
+Console.WriteLine($"Да");
+} 
+    else if(num >5)
+    {
+    Console.WriteLine($"Нет");
+    }
+    
