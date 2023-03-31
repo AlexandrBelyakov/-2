8224e00c9dc0d441ca9683cd11767ebc4f1fc430
AlexandrBelyakov <belya0309@mail.ru>
2023-03-31 22:45:00 +0300

    Задача 4 семинар 2

---
+++ задача 3 из семинара2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите Х1: ");
+double x1 = double.Parse(Console.ReadLine());
+Console.Write("Введите Y1: ");
+double y1 = double.Parse(Console.ReadLine());
+Console.Write("Введите x2: ");
+double x2 = double.Parse(Console.ReadLine());
+Console.Write("Введите Y2: ");
+double y2 = double.Parse(Console.ReadLine());
+
+
+double d = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2));
+
+Console.WriteLine($"d={d:f4}");
