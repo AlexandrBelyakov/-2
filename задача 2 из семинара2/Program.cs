fdb90118ff4952adc71c89891fed2d55c9adf95f
AlexandrBelyakov <belya0309@mail.ru>
2023-03-31 22:04:53 +0300

    задача изсеминара 2№2

---
+++ задача 2 из семинара2/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Введите переменную");
+int quater = int.Parse(Console.ReadLine());
+
+switch (quater)
+{
+    case 1:{
+        Console.WriteLine ("x>0 y>0");
+        break;
+    }
+    case 2:{
+        Console.WriteLine ("x<0 y>0");
+        break;
+    }
+    case 3:{
+        Console.WriteLine ("x<0 y<0");
+        break;
+    }
+    case 4:{
+        Console.WriteLine ("x>0 y<0");
+        break;
+    }
+    default:{
+        Console.WriteLine("Введена неправильная четверть");
+        break;
+    }
+
+}
